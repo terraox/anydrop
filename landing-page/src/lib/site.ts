b24269349f3d599439676133df2fe7fb175f4f56
AadityaBasisth <basisthaaditya@gmail.com>
2026-01-12 06:14:26 +0530

    page inital changes

---
+++ landing-page/src/lib/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "AnyDrop",
+  url: "https://anydrop.vercel.app",
+  description: "Seamless cross-device file sharing for everyone.",
+  links: {
+    twitter: "https://x.com/anydrop",
+    github: "https://github.com/anydrop",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
